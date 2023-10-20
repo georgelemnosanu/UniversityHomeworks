bd9e344bad81235cd9ff5a7d18ff5f308f145522
LMN <georgelemnosanu@gmail.com>
2023-10-20 10:59:09 +0300

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+     Tema_18_octombrie_2023 tema18Octombrie2023 = new Tema_18_octombrie_2023();
+     tema18Octombrie2023.theBigNumber(7);
+     tema18Octombrie2023.restImpartire();
+
+    }
+}
---
+++ src/main/java/org/example/Tema_18_octombrie_2023.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Tema_18_octombrie_2023 {
+
+    public int introduceNumber() {
+        System.out.println("Introdu Numarul");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public boolean theBigNumber(int num) {
+        int a = introduceNumber();
+        if (a >= num) {
+            System.out.println("Valoarea este mai mare decat 7");
+            return true;
+        } else {
+            System.out.println("Valoarea este inferioara lui 7 ");
+            return false;
+        }
+    }
+
+
+            public String restImpartire(){
+               int a = introduceNumber();
+               int rest =  a / 5;
+               String resultText;
+
+                switch (rest) {
+                    case 0:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 1:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 2:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 3:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 4:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 5:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 6:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 7:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 8:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    case 9:
+                        resultText = "Restul impartirii la 5 este " + rest;
+                        break;
+                    default:
+                        resultText = "Restul este prea mare" ;
+                        break;
+                }
+                System.out.println(resultText);
+                return resultText;
+            }
+
+
+
+
+    }
+
